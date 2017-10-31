51414d87398910285c252805d4e4426d0b06638d
Nikolay <6ajlamyt2013@users.noreply.github.com>
2017-10-31 12:23:29 +0500

    fix status code

--- parsingLog_task1.rb
+++ parsingLog_task1.rb
@@ -4,7 +4,7 @@
   status_404 = 0
   status_401 = 0
   status_503 = 0
-  time = 0
+  status_500 = 0
     file.each do |line| num+=1
 
       if line =~/up_status="200"/
@@ -13,8 +13,10 @@
         status_401 +=1
       elsif line =~/up_status="404"/
         status_404 +=1
-      elsif line =~/status_503"/
+      elsif line =~/up_status="503"/
         status_503 +=1
+      elsif line =~/up_status="500"/
+        status_500 +=1
       end
 	  
     end
@@ -22,3 +24,4 @@
   puts "401 - #{status_401}"
   puts "404 - #{status_404}"
   puts "503 - #{status_503}"
+  puts "500 - #{status_500}"
